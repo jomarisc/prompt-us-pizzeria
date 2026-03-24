3d23a0f5abc00c9f567e36d460627e4fa094cdd4
jomarisc <jomarisc@ucsc.edu>
2026-03-24 16:48:17 -0700

    push pizza

--- src/scenes/Pizzeria.js
+++ src/scenes/Pizzeria.js
@@ -4,10 +4,12 @@ export class Pizzeria extends Phaser.Scene {
     }
 
     preload(){
-
+        this.load.image('pizza-bg', './assets/pizza-bg.png');
     }
 
     create(){
+        this.add.image(960, 540, 'pizza-bg');
+
         this.add.text(960, 540, 'Welcome to the pizzeria.', {
             fontSize: '64px',
             fill: '#ce4949'
