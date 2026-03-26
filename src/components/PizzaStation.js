4ad339ff47101b3a27ee8814e91aa77e304e46d0
Luciano Lorenzana <lucianolomalor251@gmail.com>
2026-03-26 11:56:56 -0700

    feat(pizza_station): draggable object between two objects

--- src/components/PizzaStation.js
+++ src/components/PizzaStation.js
@@ -3,26 +3,38 @@ export class PizzaStation extends Phaser.GameObjects.Container {
         super(scene, x, y);
         scene.add.existing(this);
 
-        const box = scene.add.rectangle(0, 0, 800, 1000, 0xffffff);
+        // A box container for the actual pizza station
+        const box = scene.add.rectangle(0, 0, 835, 950, 0xffffff);
         box.setOrigin(0,0);
+        box.setStrokeStyle(5, 0x000000);
+        
+        const trash = scene.add.rectangle(418, 115, 800, 200, 0x8a8a8a);
+        trash.setStrokeStyle(5,0x000000);
     
-        const end = scene.add.circle(400, 500, 60,0x62a858);
+        // Our pizza that'll house the toppings
+        const end = scene.add.circle(400, 500, 200,0x62a858);
         end.setStrokeStyle(5, 0x000000);
- 
-        const circle = scene.add.circle(20, 20, 50, 0x000000);
+        
+        // Sample topping
+        const circle = scene.add.circle(100, 850, 50, 0x000000);
         circle.setInteractive({ draggable: true });
+        circle.setStrokeStyle(5, 0x000000);
 
+        // Using Math.Snap to easily move our toppings to the pizza
         scene.input.on('drag', (pointer, gameObject, dragX, dragY) => {
-            dragX = Phaser.Math.Snap.To(dragX, 20);
-            dragY = Phaser.Math.Snap.To(dragY, 20);
+            dragX = Phaser.Math.Snap.To(dragX, 64);
+            dragY = Phaser.Math.Snap.To(dragY, 64);
 
             gameObject.setPosition(dragX, dragY)
         });
         
+        // Allows to move any object that has 'draggable' set to true.
         scene.input.on('dragend', (pointer, gameObject) => {
             const x = gameObject.x;
             const y = gameObject.y;
-            if (x === 400 && y === 500) {
+            if (x < 601 && x > 199 && y < 701 && y > 299) {
+                gameObject.setFillStyle(0x00aaff);
+            } else if (x < 419 && x > 17 && y < 216 && y > 14 ) {
                 gameObject.setFillStyle(0xff0000);
             }
             else {
@@ -31,7 +43,7 @@ export class PizzaStation extends Phaser.GameObjects.Container {
         })
         
 
-        this.add([box, end, circle]);
+        this.add([box,trash, end, circle]);
     }
 
 }
