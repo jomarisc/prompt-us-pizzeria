461c3bbc0cdd9d744f4c8b51b9f0bda7d2244830
Luciano Lorenzana <lucianolomalor251@gmail.com>
2026-04-09 16:21:15 -0700

    feat(congratulation): add quirky messege below congratulations!

--- src/scenes/Congrats.js
+++ src/scenes/Congrats.js
@@ -12,7 +12,12 @@ export class Congrats extends Phaser.Scene{
             fontFamily: 'Special Elite',
             fontSize: '120px',
             // fontStyle: 'italic'
-        }).setOrigin(0.5)
+        }).setOrigin(0.5);
+
+        this.add.text(960, 400, 'Now here is your pizza!',{
+            fontFamily: 'Special Elite',
+            fontSize: '50px'
+        }).setOrigin(0.5);
 
     }
 
